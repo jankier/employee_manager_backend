60dbd6d19ae53737a962ef2c8d0087406fa2dc0d
jankier-admin <jan.kierejsza@gmail.com>
2024-08-14 13:57:38 +0200

    backend setup, created api documentation

---
+++ src/main/java/com/employee_api/springboot_bootcamp/SpringbootBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.employee_api.springboot_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBootcampApplication.class, args);
+	}
+
+}
